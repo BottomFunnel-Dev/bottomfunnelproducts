ef776b3de528220530ff9e123a9cf1a699d32e37
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-03 19:31:09 +0530

    final commit for the day

--- pages/funneldesk/index.js
+++ pages/funneldesk/index.js
@@ -8,6 +8,11 @@ import { FunnelSupport } from "../../components/funnelDeskCom/FunnelSupport";
 import { IndustryExperts } from "../../components/funnelDeskCom/IndustryExperts";
 import { useSelector } from "react-redux";
 import { ProductsNavbar } from "../../components/Navbar/ProductsNavbar/ProductsNavbar";
+import StoriesSection from "../../components/CommonComponents/StoriesSection/StoriesSection";
+import TrustedBy from "../../components/TrustedByProductsPage/TrustedBy";
+import LifeBottomFunnel from "../../components/ProductsNewPage/LifeBottomFunnel/LifeBottomFunnel";
+import { GetStarted } from "../../components/ProductsNewPage/GetStarted/GetStarted";
+import { ProductsFooter } from "../../components/ProductsNewPage/ProductsFooter/ProductsFooter";
 
 export default function FunnelDesk() {
   const productMount = useSelector((state) => state.mountNav);
@@ -23,17 +28,25 @@ export default function FunnelDesk() {
           logo: false,
         }}
       />
-      <ProductsNavbar  navproductredux={{
-            color: "black",
-            logo: false,
-          }}/>
-          
+      <ProductsNavbar
+        navproductredux={{
+          color: "black",
+          logo: false,
+        }}
+      />
+
       <FunnelDeskBanner />
       <FunnelSupport />
       <EliteServices />
       <EliteLast />
       <IndustryExperts />
       <DeskStarted />
+
+      <StoriesSection />
+      <TrustedBy />
+      <LifeBottomFunnel />
+      <GetStarted />
+      <ProductsFooter />
     </div>
   );
 }
