15084c1b4df6dce4cd284292d2e243bef2d65f6a
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-04 10:53:34 +0530

    code changes

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
         />
 
         <ProductsNewPageBanner />
-        <MultipleProducts />
+        {/* <MultipleProducts /> */}
         <TalentNetwork />
         <SimpleAndEasy />
         <TalentProfiles />
