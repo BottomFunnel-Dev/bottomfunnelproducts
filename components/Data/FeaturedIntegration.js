a142a6fe8a2a05831cf72faefa43452050d1cb9a
jyotiranjan98 <jyoti.das955@gmail.com>
2023-02-06 20:48:32 +0530

    changes in integration

---
+++ components/Data/FeaturedIntegration.js
@@ -0,0 +1,58 @@
+export const FeaturedIntegration ={
+    integrationsList: [
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/whatsapp-logo-png-2263 1.png",
+                alt: "whatsapplogo"
+            },
+            title: "Whatsapp for Freshdesk",
+            para: "Engage your customers on the world's most popular messaging app with our WhatsApp integration for freshdesk Connect WhatsApp with Freshdesk to seamlessly support customers on the most popular messaging app",
+            btn: "Sign Up For Free"
+        },
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/slack-logo-icon 1.png",
+                alt: "slacklogo"
+            },
+            title: "Freshdesk-Slack Integration",
+            para: "Enable frictionless cross-team communication by integrating Freshdesk's customer support software with the team collaboration capabilites of Slack",
+            btn: "Sign Up For Free"
+        },
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/1738792 1.png",
+                alt: "shopifylogo"
+            },
+            title: "Build better customer relationships with Freshdesk and andegating your help desk software with Shopify saves a lot of back and furth",
+            para: "Engage your customers on the world's most popular messaging app with our WhatsApp integration for freshdesk Connect WhatsApp with Freshdesk to seamlessly support customers on the most popular messaging app",
+            btn: "Sign Up For Free"
+        },
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/58482f67cef1014c0b5e4a81 1.png",
+                alt: "salesforcelogo"
+            },
+            title: "Whatsapp for Freshdesk",
+            para: "Engage your customers on the world's most popular messaging app with our WhatsApp integration for freshdesk Connect WhatsApp with Freshdesk to seamlessly support customers on the most popular messaging app",
+            btn: "Sign Up For Free"
+        },
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/5e8cdf0a664eae000408545b 1.png",
+                alt: "microsoftteamlogo"
+            },
+            title: "Simplify cross-team collaboration and enable contextual customer support with Microsoft Teams app",
+            para: "The Microsoft Teams app for Freshdesk makes cross-team collaboration effortless and frictionless. With this integration, stay updated on the tickets assigned to you in Freshdesk, quickly share support tasks with any team, and resolve issues faster.",
+            btn: "Sign Up For Free"
+        },
+        {
+            img: {
+                src: "/Images/featuredintegration/integrationimages/Group.png",
+                alt: "mailchimplogo"
+            },
+            title: "Freshdesk-MailChimp Integration",
+            para: "Customize your mailing lists and send targeted email campaigns to customers, right from your helpdesk",
+            btn: "Sign Up For Free"
+        }
+    ]
+}
--- components/FeaturedIntegrationPage/FeaturedIntegration/FeaturedIntegration.jsx
+++ components/FeaturedIntegrationPage/FeaturedIntegration/FeaturedIntegration.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import style from "./featuredintegration.module.css"
+
+
 export const FeaturedIntegration = () => {
   return (
-    <div>
+    <div className={style.FeaturedIntegrationMain}>
         <div className={style.FeaturedIntegrationLine}>
             <p>Featured Integrations</p>
-            </div>
+        </div>
+        <div className={style.integrationsDiv}>
+        {integrationsList.map(({img,title,para,btn}) =>{
+            return(
+                <div>
+                <img src={img.src} alt={img.alt}></img>
+                <h2>{title}</h2>
+                <p>{para}</p>
+                <button>{btn}</button>
+                </div>
+            );
+        })}
+           
+        </div>
     </div>
   )
 }
