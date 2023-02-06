f37a20557c375785d4acca19b923b23b9e11bf49
jyotiranjan98 <jyoti.das955@gmail.com>
2023-02-06 19:38:48 +0530

    changes in integration

---
+++ components/FeaturedIntegrationPage/Integrationfooter/IntegrationFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from "./IntegrationFooter.module.css"
+
+export const IntegrationFooter = () => {
+  return (
+    <div className={style.IntegrationFooterMain}>
+        <div className={style.IntegrationFootercontain}>
+        <h1>Start today, and enjoy our free trial.</h1>
+        <p>start your 21-days free trial. No credit card required. No strings attached.</p>
+        <button className={style.IntegrationFooterbutton1}>Start Free Trial</button>
+        <button className={style.IntegrationFooterbutton2}>Request Demo</button>
+        </div>
+    </div>
+  )
+}
