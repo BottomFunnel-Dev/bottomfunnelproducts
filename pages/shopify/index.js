289df4ca293d46e743e50cf3859065531fd27cab
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-23 18:53:00 +0530

    changes

--- pages/shopify/index.js
+++ pages/shopify/index.js
@@ -3,6 +3,17 @@ import Navbar from '../../components/Navbar/Navbar'
 import { Shopifybanner } from '../../components/Shopify/Shopifybanner/Shopifybanner'
 import { Shopifyframes } from '../../components/Shopify/ShopifyFrames/Shopifyframes'
 import { ShopifyIntigration } from '../../components/Shopify/ShopifyIntigration/ShopifyIntigration'
+import { Shopifylowerbanner } from '../../components/Shopify/Shopifylowerbanner/Shopifylowerbanner'
+import OndemandSection from '../../components/CommonComponents/OnDemandSection/OndemandSection'
+import PlanProject from '../../components/CommonComponents/PlanProject/PlanProject'
+import { SeoblogSection } from '../../components/CommonComponents/SeoBlogs/Seoblogs'
+import Revamping from '../../components/revamping/Revamping'
+import { TextDropdown } from '../../components/CommonComponents/faqSection/Faqs'
+import ContectForm from '../../components/CommonComponents/ContectForm/ContectForm'
+import MainFooter from '../../components/CommonComponents/FooterSection/MainFooter'
+import StoriesSection from '../../components/CommonComponents/StoriesSection/StoriesSection'
+
+
 
 export default function Shopify  () {
   return (
@@ -20,6 +31,17 @@ export default function Shopify  () {
     <Shopifybanner />
     <ShopifyIntigration />
     <Shopifyframes />
+    <Shopifylowerbanner />
+
+    <StoriesSection />
+    <OndemandSection />
+    {/* <PortFolio />  */}
+    <PlanProject />
+    <SeoblogSection />
+    <Revamping />
+    <TextDropdown />
+    <ContectForm />
+    <MainFooter />
     </div>
   )
 }
