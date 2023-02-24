6b5be3b765383095e50efb67b454084d0711a4a1
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-24 15:17:10 +0530

    changes

---
+++ components/Messaginchannel/Messaginchannelbanner.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+import styles from "./Messaginchannelbanner.module.css"
+
+
+export const Messaginchannelbanner = () => {
+  return (
+    <div className={styles.messaginchannelbannermainboxes}>
+    <div className={styles.messagingchannelbannerleft}>
+    
+    <Image
+          src={"/Images/Messaging channels/components/Group 1000006011.webp"}
+          alt={"images"}
+          width={"0"}
+          height={"0"}
+          sizes={"100vw"}
+          style={{ width: "100%", height: "100%" }}
+        />
+    
+    </div>
+
+    <div className={styles.messagingchannelbannerright}>
+    <h2>Engage with <span>Customers</span> across messaging channels</h2>
+    <p>scale communication across channels and delight your customers at every touchpoint.</p>
+    <button className={styles.messaginchannelbannerbuton}>Explore Now</button>
+    </div>
+    
+    
+    </div>
+  )
+}
