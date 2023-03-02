236e7c9c6fc1e8e0369afb9e7de349baf1271ada
Aaradhana29 <kumariaaradhana99@gmail.com>
2023-03-02 17:48:21 +0530

    changes

--- pages/landing-pages/index.js
+++ pages/landing-pages/index.js
@@ -6,18 +6,94 @@ import { AIPowered } from "../../components/Landing-Pages/AIPowered/AIPowered";
 import { LandingBrand } from "../../components/Landing-Pages/LandingBrand/LandingBrand";
 import { LandingCustomer } from "../../components/Landing-Pages/LandingCustomer/LandingCustomer";
 import { LandingSales } from "../../components/Landing-Pages/LandingSales/LandingSales";
+import Navbar from "../../components/Navbar/Navbar";
+import { ProdNavbar } from "../../components/ProdNavbar";
+
+const productData = {
+  title: "Bottom Funnel Marketer",
+  logo: "/Images/navbar/product/Market.png",
+  width: "45%",
+  list1: {
+    title: "Features",
+    more: { title: "View more features", path: "/404" },
+    position: { right: "35%" },
+    products: [
+      {
+        dropContent: "Multichannel Engagement",
+        path: "/404",
+      },
+      {
+        dropContent: "Email & SMS",
+        path: "/404",
+      },
+      {
+        dropContent: "WhatsApp",
+        path: "/404",
+      },
+      {
+        dropContent: "Journeys",
+        path: "/404",
+      },
+      {
+        dropContent: "Landing Pages",
+        path: "/landing-pages",
+      },
+      {
+        dropContent: "Shopify Integration",
+        path: "/404",
+      },
+    ],
+  },
+  list4: {
+    title: "Resources",
+    more: false,
+    position: { right: "20%" },
+    products: [
+      {
+        dropContent: "Product Tours",
+        path: "/404",
+      },
+      {
+        dropContent: "Case Studies",
+        path: "/404",
+      },
+      {
+        dropContent: "What's new",
+        path: "/404",
+      },
+      {
+        dropContent: "Guide to Shopify",
+        path: "/404",
+      },
+    ],
+  },
+  pricing: "",
+};
+
 
 export default function landingPages() {
     return (
     <div>
-  <LandingBanner/>
- <LandingFeature/>
- <LandingTrafic/>
- <LandingSolution/>
- <AIPowered/>
- <LandingBrand/>
- <LandingCustomer/>
- <LandingSales/>
+
+    <Navbar
+        productMount={{
+          navMount: true,
+        }}
+        navredux={{
+          color: "black",
+          logo: false,
+        }}
+      />
+      <ProdNavbar data={productData} />
+
+      <LandingBanner/>
+      <LandingFeature/>
+      <LandingTrafic/>
+      <LandingSolution/>
+      <AIPowered/>
+      <LandingBrand/>
+      <LandingCustomer/>
+      <LandingSales/>
     </div>
     );
 }
