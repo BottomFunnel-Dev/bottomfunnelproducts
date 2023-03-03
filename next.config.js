99bdd39a72ea2dc501b0eceeefba115d49ce57e7
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-03-03 16:41:56 +0530

    config changees

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   distDir: "build",
-// };
-
-// module.exports = nextConfig;
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
-
   async redirects() {
     return [
       {
@@ -30,10 +18,45 @@ module.exports = withBundleAnalyzer({
       },
       {
         source: "/wearable-app-development",
-        destination:"https://service.bottomfunnel.net/wearable-app-development",
+        destination:
+          "https://service.bottomfunnel.net/wearable-app-development",
         permanent: false,
         basePath: false,
       },
     ];
   },
-});
+};
+
+module.exports = nextConfig;
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   distDir: "build",
+
+//   async redirects() {
+//     return [
+//       {
+//         source: "/django",
+//         destination: "https://service.bottomfunnel.net/skills/django",
+//         permanent: true,
+//         basePath: false,
+//       },
+//       {
+//         source: "/pharmacy",
+//         destination: "https://service.bottomfunnel.net/pharmacy",
+//         permanent: false,
+//         basePath: false,
+//       },
+//       {
+//         source: "/wearable-app-development",
+//         destination:"https://service.bottomfunnel.net/wearable-app-development",
+//         permanent: false,
+//         basePath: false,
+//       },
+//     ];
+//   },
+// });
