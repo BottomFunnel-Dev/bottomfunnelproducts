5fc3f17ea8b9a584b8c344642547a82f2b0a91a1
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-03-03 18:08:29 +0530

    added smb file

--- pages/smb/index.js
+++ pages/smb/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { ProductsNavbar } from '../../components/Navbar/ProductsNavbar/ProductsNavbar'
+import LifeBottomFunnel from '../../components/ProductsNewPage/LifeBottomFunnel/LifeBottomFunnel'
+import { ProductsFooter } from '../../components/ProductsNewPage/ProductsFooter/ProductsFooter'
+import ReadyStarted from '../../components/ProductsNewPage/ReadyStarted/ReadyStarted'
+import Testimonial from '../../components/ProductsNewPage/Testimonial/Testimonial'
 import { Smbbanner } from '../../components/SMB/Smbbanner'
+import { Smbcustomerservice } from '../../components/SMB/Smbcustomerservice'
+import { Smbfooter } from '../../components/SMB/Smbfooter'
+import TrustedBy from '../../components/TrustedByProductsPage/TrustedBy'
 
 export default function SMB  ()  {
   return (
@@ -15,9 +23,22 @@ export default function SMB  ()  {
     }}
   />
 
+  <ProductsNavbar
+  navproductredux={{
+    color: "black",
+    logo: false,
+  }}
+/>
+
     <Smbbanner />
-    
-    
+    <Smbcustomerservice />
+    <Smbfooter />
+
+    <TrustedBy />
+    <Testimonial />
+    <ReadyStarted />
+    <LifeBottomFunnel />
+    <ProductsFooter />
     
     </div>
   )
