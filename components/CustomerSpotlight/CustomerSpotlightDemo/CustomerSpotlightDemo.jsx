e9aa15728bfbb594a38f31a3bff9258d31591724
NeerajMalviya1997 <nmalviya336@gmail.com>
2023-03-04 15:28:53 +0530

    changes done in guide to shopify ,customer spotlight done,market place

---
+++ components/CustomerSpotlight/CustomerSpotlightDemo/CustomerSpotlightDemo.jsx
@@ -0,0 +1,11 @@
+import styles from "./CustomerSpotlightDemo.module.css"
+
+export const CustomerSpotlightDemo = () => {
+    return (
+    <div className={styles.CustomerSpotlightDemoParent}>
+  <h1>Our Case Studies Showcase Best
+Suggestions And Tips</h1>
+<p>Start your 21-day free trial. No credit card required.</p>
+<button>Start Demo</button>
+    </div>)
+}
--- components/GuideToShopFy/GuideToShopFyConfigure/GuideToShopFyConfigure.jsx
+++ components/GuideToShopFy/GuideToShopFyConfigure/GuideToShopFyConfigure.jsx
@@ -3,80 +3,89 @@ import Image from "next/image"
 export const GuideToShopFyConfigure = () => {
     return (
         <div className={styles.GuideToShopFyConfigureaPrent}>
-            <h1>Configure your account</h1>
+            <div className={styles.GuideToShopFyConfigureHeading}>
+                <h1>Configure your account</h1>
             <p>Set up your CRM the way you prefer. Invite your teammates and set up roles and permissions.
-                Customize data you want to see, lifecycle stages, lead scoring, chat widget, and more.</p>
+                Customize data you want to see, lifecycle stages, lead scoring, chat widget, and more.</p></div>
+            
             <div className={styles.GuideToShopFyConfigureOnlyChild}>
-                <div className={styles.GuideToShopFyConfigureOnlyChilddivone}>
-                    <div>
+            <div className={styles.lineone}></div>
+                <div className={styles.GuideToShopFyConfigureOnlyChildinsidefirstdiv}>
+                    <div className={styles.GuideToShopFyConfigureOnlyChilddivone}>
+                        <div>
 
-                        <h1>Invite Teammates </h1>
-                        <Image
-                            src={"/Images/GuideToShopFy/icon1.png"}
-                            alt={"img"}
-                            width={"0"}
-                            height={"0"}
-                            sizes={"100vw"}
-                            style={{ width: "15%", height: "15%" }}
+                            <h3>Invite Teammates </h3>
+                            <Image
+                                src={"/Images/GuideToShopFy/icon1.png"}
+                                alt={"img"}
+                                width={"0"}
+                                height={"0"}
+                                sizes={"100vw"}
+                                style={{ width: "15%", height: "15%" }}
 
-                        />
+                            />
+
+                        </div>
+
+                        <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
 
                     </div>
+                    <div className={styles.GuideToShopFyConfigureOnlyChilddivtwo}>
+                        <div>
 
-                    <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
+                            <h3>Verify your email and domain </h3>
+                            <Image
+                                src={"/Images/GuideToShopFy/icon2.png"}
+                                alt={"img"}
+                                width={"0"}
+                                height={"0"}
+                                sizes={"100vw"}
+                                style={{ width: "15%", height: "15%" }}
 
-                </div>
-                <div className={styles.GuideToShopFyConfigureOnlyChilddivtwo}>
-                    <div>
+                            />
+                        </div>
 
-                        <h1>Verify your email and domain </h1>
-                        <Image
-                            src={"/Images/GuideToShopFy/icon2.png"}
-                            alt={"img"}
-                            width={"0"}
-                            height={"0"}
-                            sizes={"100vw"}
-                            style={{ width: "15%", height: "15%" }}
-
-                        />
+                        <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
                     </div>
+                    <div className={styles.GuideToShopFyConfigureOnlyChilddivthree} >
+                        <div>
 
-                    <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
-                </div>
-                <div className={styles.GuideToShopFyConfigureOnlyChilddivthree} >
-                    <div>
+                            <h3>Manage Customers</h3>
+                            <Image
+                                src={"/Images/GuideToShopFy/icon3.png"}
+                                alt={"img"}
+                                width={"0"}
+                                height={"0"}
+                                sizes={"100vw"}
+                                style={{ width: "15%", height: "15%" }}
 
-                        <h1>Manage Customers</h1>
-                        <Image
-                            src={"/Images/GuideToShopFy/icon3.png"}
-                            alt={"img"}
-                            width={"0"}
-                            height={"0"}
-                            sizes={"100vw"}
-                            style={{ width: "15%", height: "15%" }}
+                            />
+                        </div>
 
-                        />
+                        <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
                     </div>
-
-                    <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
                 </div>
-                <div className={styles.GuideToShopFyConfigureOnlyChilddivfour} >
-                    <div>
+                <div className={styles.GuideToShopFyConfigureOnlyChildinsidesecdiv}>
+                    <div className={styles.GuideToShopFyConfigureOnlyChilddivfour} >
+                        <div>
 
-                        <h1>Create shopify lists and segments </h1>
-                        <Image
-                            src={"/Images/GuideToShopFy/icon3.png"}
-                            alt={"img"}
-                            width={"0"}
-                            height={"0"}
-                            sizes={"100vw"}
-                            style={{ width: "15%", height: "15%" }}
+                            <h3>Create shopify lists and segments </h3>
+                            <Image
+                                src={"/Images/GuideToShopFy/icon3.png"}
+                                alt={"img"}
+                                width={"0"}
+                                height={"0"}
+                                sizes={"100vw"}
+                                style={{ width: "15%", height: "15%" }}
 
-                        />
+                            />
+                        </div>
+
+                        <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
                     </div>
-
-                    <p>IF you have an existing CRM account ,you can connect it with shopify from the freshwork marketplace .you can also set up integration from shopify app store</p>
                 </div>
+                
+            <div className={styles.line}></div>
             </div>
 
         </div>
