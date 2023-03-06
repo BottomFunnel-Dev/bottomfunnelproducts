cd05fb5820fee62e7e666b0549f7a66032a4bbba
Aaradhana29 <kumariaaradhana99@gmail.com>
2023-03-06 15:59:42 +0530

    changes

--- components/funnelDeskEnterprise/EnterpriseDeskBanner.jsx
+++ components/funnelDeskEnterprise/EnterpriseDeskBanner.jsx
@@ -16,7 +16,7 @@ export const EnterpriseDeskBanner = () => {
       </div>
       <div className={styles.enterBanner}>
         <img
-          src="/Images/Enterprise/banner/Banner.png"
+          src="/Images/Enterprise/banner/Banner.webp"
           alt="banner/Banner.png"
         />
       </div>
