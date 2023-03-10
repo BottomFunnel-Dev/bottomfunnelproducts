4ae7763db5e1e65bff91d3cf2393dbec9502190c
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-03-10 13:49:07 +0530

    error resolution

--- pages/landing-pages/index.js
+++ pages/landing-pages/index.js
@@ -1,7 +1,7 @@
 import { LandingFeature } from "../../components/Landing-Pages/Feature/LandingFeature";
 import { LandingBanner } from "../../components/Landing-Pages/Banner/LandingBanner";
 import { LandingTrafic } from "../../components/Landing-Pages/LandingTrafic/LandingTrafic";
-import { LandingSolution } from "../../components/Landing-Pages/LandingSolution/Landingsolution";
+import { LandingSolution } from "../../components/Landing-Pages/LandingSolution/LandingSolution";
 import { AIPowered } from "../../components/Landing-Pages/AIPowered/AIPowered";
 import { LandingBrand } from "../../components/Landing-Pages/LandingBrand/LandingBrand";
 import { LandingCustomer } from "../../components/Landing-Pages/LandingCustomer/LandingCustomer";
