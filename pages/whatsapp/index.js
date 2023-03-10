744c0443029a14ee46d960e1b9a54ebf60ccb76b
Jyotiranjan Das <96143764+jyotiranjan98@users.noreply.github.com>
2023-03-10 10:31:50 +0530

    path added

--- pages/whatsapp/index.js
+++ pages/whatsapp/index.js
@@ -64,7 +64,7 @@ const productData = {
       },
       {
         dropContent: "What's new",
-        path: "/",
+        path: "/whatsnew",
       },
       {
         dropContent: "Guide to Shopify",
