39485cfbcde199e870beec3747db404d5b61e6b3
Aaradhana29 <kumariaaradhana99@gmail.com>
2023-03-14 19:22:21 +0530

    changes

--- components/AllIndustries/AllIndustriesBanner.jsx
+++ components/AllIndustries/AllIndustriesBanner.jsx
@@ -10,6 +10,7 @@ export const AllIndustriesBanner = () => {
     <p>Customer support impacts business growth and revenue immensely, irrespective of the industry you operate in.Freshdesk allows you to extend a consistent,seamless,and unified service experience to all your customers.Being incredibly easy to set up and use,Freshdesk is robust customer service software that can be customized for your industry needs.</p>
 
     <button className={styles.allindustriesbannerbutton}>Start A Free Demo</button>
+    <button className={styles.allindustriesbannerbutton1}>Request Demo</button>
     </div>
     
     
--- components/funnelDeksTicketing/TickettingInbox/TickettingInbox.jsx
+++ components/funnelDeksTicketing/TickettingInbox/TickettingInbox.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./TickettingInbox.module.css";
+import Image from 'next/image';
 
 export const TickettingInbox = () => {
   return (
-    <div className={styles.tickettingInboxConatiner}>
+    <div className={styles.tickettingInboxContainer}>
       <div className={styles.ticketInboxHeading}>
         <h2>Team Inbox</h2>
         <p>
@@ -13,6 +14,116 @@ export const TickettingInbox = () => {
           Assign every ticket to a specific agent and group so there is no
           confusion about who should be working on which ticket.
         </p>
+      </div>
+      <div className={styles.tickettingInboxDataDiv}>
+          <div className={styles.tickettingInboxData}>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Collision detection
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                  See who else is viewing or replying to the ticket.
+              </div>
+              <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow1.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+          </div>
+          <div className={styles.tickettingInboxData1}>
+             <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow2.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Canned responses
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                  Save replies to common tickets and reuse them.
+              </div>
+          </div>
+          <div className={styles.tickettingInboxData}>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Smart notifications
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                  Get notified within the tool about ticket updates.
+              </div>
+              <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow1.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+          </div>
+          <div className={styles.tickettingInboxData1}>
+             <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow2.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Custom ticket views
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                 Choose whichh tickets you want to see first.
+              </div>
+          </div>
+          <div className={styles.tickettingInboxData}>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Merging tickets
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                  Merge tickets about the same issues into one.
+              </div>
+              <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow1.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+          </div>
+          <div className={styles.tickettingInboxData1}>
+             <div className={styles.tickettingInboxDataDiv3}>
+              <Image
+                src={"/Images/ticketingPhotos/Arrow2.png"}
+                alt={"icon"}
+                width={"0"}
+                height={"0"}
+                sizes={"100vw"}
+                style={{ width: "100%", height: "100%" ,objectFit:"Contain"}}
+              />        
+              </div>
+              <div className={styles.tickettingInboxDataDiv1}>
+                 Activity log
+              </div>
+              <div className={styles.tickettingInboxDataDiv2}>
+                  View every agent and system activity on ticket.
+              </div>
+          </div>
       </div>
     </div>
   );
