e7dc3dd7081b0ca9b06f9fd6e35563aa04080fa6
mayra111 <micgupta01@gmail.com>
2023-03-15 18:28:32 +0530

    added

--- components/SalesPage/SalesPageBanner/SalesPageBanner.jsx
+++ components/SalesPage/SalesPageBanner/SalesPageBanner.jsx
@@ -7,17 +7,8 @@ export const SalesPageBanner = () => {
     <div>
       <div className={style.SalesPageBannermainbox}>
         <div className={style.SalesPageBannermainboximage}>
-        {/* <Image
-          src={"/Images/SalesPagePhotos/salesbnaer.webp"}
-          alt={"/Images/SalesPagePhotos/salesbnaer.webp"}
-          width={"0"}
-          height={"0"}
-          sizes={"100vw"}
-          style={{ width: "100%", height: "100%" }}
-        /> */}
-          {/* <img src="/Images/SalesPagePhotos/salesbnaer.png"></img> */}
-
-          <lottie-player
+       
+             <lottie-player
                   src={"/Animation/saleshomepage/sales.json"}
                   background="transparent"
                   speed="1"
