997ec573edf870448e4d384fa3b242d936305113
mayra111 <micgupta01@gmail.com>
2023-03-16 15:45:06 +0530

    completed

---
+++ components/BottomFunnelTeam/Implementation/Implementation.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from "./Implementation.module.css";
+
+export const Implementation = () => {
+  return (
+    <div>
+        <div className={styles.Implementationmaindiv}>
+           <div className={styles.Implementationmaindivtext}>
+            <h2>Open HRMS Implementation </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore repellendus sed natus similique dignissimos odio neque quo dolorem id non, omnis voluptatibus qui tempora fugiat dolorum ad laudantium nam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus aspernatur tempore ab ipsa natus distinctio voluptatibus unde! Optio, assumenda doloremque sequi dolorum eaque nemo tempore soluta corporis, unde aut culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, impedit qui totam dicta consequatur quaerat odio tempora reiciendis tenetur? Error doloremque ullam aperiam hic. Rerum voluptate laboriosam voluptatem modi asperiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis harum est non temporibus earum voluptatem? Cumque, debitis tempora? Nam temporibus et velit asperiores iusto unde porro neque, repellat nisi!</p>
+            <button className={styles.Implementationmaindivbutton}>View Packages </button>
+        </div>
+
+
+        <div className={styles.Implementationinnerdiv}>
+            <div  className={styles.Implementationinnerdivcontent}>
+                <h1>AUTOMATE</h1>
+                <p> debitis recusandae laboriosam maiores perspiciatis quas odio animi consectetur eos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit natus fuga culpa praesentium molestias atque, magnam accusamus unde, officiis iusto ea rem, aliquid ipsam at doloribus et hic itaque provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fugit ad iure omnis nobis aliquid rem eum in doloremque quod exercitationem minima nemo placeat non quam? Iste, sunt? Eaque, accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus vero et eligendi iusto alias nemo dolore consectetur, quam maxime fuga in voluptate voluptatum perferendis suscipit excepturi aperiam quae? Veritatis?</p>
+            </div>
+            <div className={styles.Implementationinnerdivimage}>
+            <Image
+  src={"/Images/bottomfunnelteamsphotos/screen1.webp"}
+  alt={"image"}
+  width={"0"}
+  height={"0"}
+  sizes={"100vw"}
+  style={{ width: "100%", height: "100%" }}
+
+  />
+            </div>
+        </div>
+
+
+        <div className={styles.Implementationinnerdivone}>
+            <div  className={styles.Implementationinnerdivcontentone}>
+                <h1>STRATEGIC MANAGEMENT</h1>
+                <p> debitis recusandae laboriosam maiores perspiciatis quas odio animi consectetur eos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit natus fuga culpa praesentium molestias atque, magnam accusamus unde, officiis iusto ea rem, aliquid ipsam at doloribus et hic itaque provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fugit ad iure omnis nobis aliquid rem eum in doloremque quod exercitationem minima nemo placeat non quam? Iste, sunt? Eaque, accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus vero et eligendi iusto alias nemo dolore consectetur, quam maxime fuga in voluptate voluptatum perferendis suscipit excepturi aperiam quae? Veritatis?</p>
+            </div>
+            <div className={styles.Implementationinnerdivimage}>
+            <Image
+  src={"/Images/bottomfunnelteamsphotos/screen2.webp"}
+  alt={"image"}
+  width={"0"}
+  height={"0"}
+  sizes={"100vw"}
+  style={{ width: "100%", height: "100%" }}
+
+  />
+            </div>
+        </div>
+
+
+        <div className={styles.Implementationinnerdiv}>
+            <div  className={styles.Implementationinnerdivcontent}>
+                <h1>EMPLOYEE ENGAGEMENT</h1>
+                <p> debitis recusandae laboriosam maiores perspiciatis quas odio animi consectetur eos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit natus fuga culpa praesentium molestias atque, magnam accusamus unde, officiis iusto ea rem, aliquid ipsam at doloribus et hic itaque provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fugit ad iure omnis nobis aliquid rem eum in doloremque quod exercitationem minima nemo placeat non quam? Iste, sunt? Eaque, accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus vero et eligendi iusto alias nemo dolore consectetur, quam maxime fuga in voluptate voluptatum perferendis suscipit excepturi aperiam quae? Veritatis?</p>
+            </div>
+            <div className={styles.Implementationinnerdivimage}>
+            <Image
+  src={"/Images/bottomfunnelteamsphotos/screen3.webp"}
+  alt={"image"}
+  width={"0"}
+  height={"0"}
+  sizes={"100vw"}
+  style={{ width: "100%", height: "100%" }}
+
+  />
+            </div>
+        </div>
+
+
+        <div className={styles.Implementationinnerdivone}>
+            <div  className={styles.Implementationinnerdivcontentone}>
+                <h1>ANALYZE AND IMPROVE</h1>
+                <p> debitis recusandae laboriosam maiores perspiciatis quas odio animi consectetur eos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit natus fuga culpa praesentium molestias atque, magnam accusamus unde, officiis iusto ea rem, aliquid ipsam at doloribus et hic itaque provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fugit ad iure omnis nobis aliquid rem eum in doloremque quod exercitationem minima nemo placeat non quam? Iste, sunt? Eaque, accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti possimus vero et eligendi iusto alias nemo dolore consectetur, quam maxime fuga in voluptate voluptatum perferendis suscipit excepturi aperiam quae? Veritatis?</p>
+            </div>
+            <div className={styles.Implementationinnerdivimage}>
+            <Image
+  src={"/Images/bottomfunnelteamsphotos/screen4.webp"}
+  alt={"image"}
+  width={"0"}
+  height={"0"}
+  sizes={"100vw"}
+  style={{ width: "100%", height: "100%" }}
+
+  />
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
