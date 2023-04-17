88527715cbc02fcb1ec04c06840c83bc6cfd4537
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-04-17 19:18:32 +0530

    changes

---
+++ components/ContactUS/ContactUsMap/ContactUsMap.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from "./ContactUsMap.module.css";
+
+import GlobalCountries from './GlobalCountires';
+
+import Positions from "./Positions"
+
+const ContactUsMap = () => {
+    return (
+        <>
+            <div>
+                <GlobalCountries />
+            </div>
+
+            <div>
+
+                {/* <Positions /> */}
+
+                <div className={styles.contactUsMapDiv}>
+                    <lottie-player
+                        src="https://bottom-funnel-fe.s3.ap-south-1.amazonaws.com/Animation/Bottom funnel footer map animation.json"
+                        background="transparent"
+                        speed="1"
+                        loop
+                        autoplay
+                    ></lottie-player>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ContactUsMap;
